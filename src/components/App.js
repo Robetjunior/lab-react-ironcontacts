37de90a5ab381b273df0d9e2bfb0cdd36a64ac0b
Jose Roberto <juniorferreirarob@gmail.com>
2020-07-23 11:38:58 -0300

    done

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Contacs from './Contacts';
+
+
+
+function App() {
+  return (
+    <div>
+      <Contacs />
+    </div>
+  );
+}
+
+export default App;
